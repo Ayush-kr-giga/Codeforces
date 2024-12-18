1460483f5c5ade07c5d8e7b3a9ab708fc3c8efdc
Ayush-kr-giga <kumarayushsharma2@gmail.com>
2024-12-18 19:58:08 +0530

    edited solution for A

--- Codeforces Round 993 (Div. 4)/A - Easy Problem/solution.py
+++ Codeforces Round 993 (Div. 4)/A - Easy Problem/solution.py
@@ -3,7 +3,7 @@ for cases in range(t):
     n=int(input())
     count=0
     for i in range(1,n):
-        for j in range(1,n):
+        for j in range(1,n):  #creating two pointers through the range
             if i+j==n:
                 count+=1
     print(count)   
